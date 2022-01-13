29a06a4e7cebf93fe229b53c8dc9e39a7b796701
Michał Płużek <michalpluzek@gmail.com>
2022-01-13 10:44:24 +0100

    feat: Add foo team generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   newMemberName = '';
-  members: string[] = [];
-  numberOfTeams: number | null = null;
+  members: string[] = ['1', '2', '3', '4', '5', '6', '7'];
+  numberOfTeams: number = 0;
+  teams: string[][] = [];
 
   addMember() {
     this.members.push(this.newMemberName);
     this.newMemberName = '';
   }
 
-  generateTeams() {}
+  generateTeams() {
+    const memberList = [...this.members];
+    const memberInTeams = memberList.length / this.numberOfTeams;
+    const maxNumberOfMember = Math.ceil(memberInTeams);
+    const minNumberOfMember = Math.floor(memberInTeams);
+    let isMaxNumberOfMemberInTeam = false;
+
+    for (let i = 0; i < this.numberOfTeams; i++) {
+      this.teams.push([]);
+
+      if (!isMaxNumberOfMemberInTeam) {
+        for (let idx = 0; idx < maxNumberOfMember; idx++) {
+          const randomMemberIndex = Math.floor(
+            Math.random() * memberList.length
+          );
+          const randomMember = memberList[randomMemberIndex];
+          memberList.splice(randomMemberIndex, 1);
+
+          this.teams[i].push(randomMember);
+        }
+
+        isMaxNumberOfMemberInTeam = true;
+      } else {
+        for (let idx = 0; idx < minNumberOfMember; idx++) {
+          const randomMemberIndex = Math.floor(
+            Math.random() * memberList.length
+          );
+          const randomMember = memberList[randomMemberIndex];
+          memberList.splice(randomMemberIndex, 1);
+
+          this.teams[i].push(randomMember);
+        }
+      }
+    }
+    console.log(this.teams);
+  }
 }
