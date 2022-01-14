49023c36ae9da4e49fd604ccd0cb103c48879699
Michał Płużek <michalpluzek@gmail.com>
2022-01-14 10:06:09 +0100

    feat: split code to outside component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TeamComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
